5abdd54c5dfb279ba1d6f57f96e5c621c3fa20d7
Monarch <monarchmaisuriya7600@gmail.com>
2019-07-25 19:05:07 +0530

    Added EJS

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
+// Server
+// load the things we need
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 8000;
+// set the view engine to ejs
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  // use res.render to load up an ejs view file
+  // index page
+  res.render("/pages/index");
+});
 
-app.get("/", (req, res) => res.send("Hello World!"));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`Example app listening on port ${port}`));
