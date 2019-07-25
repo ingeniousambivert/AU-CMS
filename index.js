c5d2938d5c57cbd2d79e45b670d139760878526c
Monarch <monarchmaisuriya7600@gmail.com>
2019-07-25 20:10:23 +0530

    Added favicon

--- index.js
+++ index.js
@@ -6,11 +6,11 @@ const port = 8000;
 // Set the view engine to ejs
 app.set("view engine", "ejs");
 // Use CSS
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 app.get("/", (req, res) => {
   // use res.render to load up an ejs view file
   // index page
   res.render("pages/index");
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}`));
+app.listen(port, () => console.log(`Server running on ${port}`));
