174a31931f4bee06edb1acb40bd8e5a8b2b107c6
Monarch <monarchmaisuriya7600@gmail.com>
2019-07-25 20:01:28 +0530

    Added configurations for static files

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // Server
-// load the things we need
+// Load the things we need
 const express = require("express");
 const app = express();
 const port = 8000;
-// set the view engine to ejs
+// Set the view engine to ejs
 app.set("view engine", "ejs");
+// Use CSS
+app.use(express.static(__dirname + '/public'));
 app.get("/", (req, res) => {
   // use res.render to load up an ejs view file
   // index page
-  res.render("/pages/index");
+  res.render("pages/index");
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}`));
